6a3a126545ff9daf8e23e8980496e2c159130ccb
hugopedro <hugopedro098@gmail.com>
2019-10-29 12:27:31 -0300

    Inserindo lista de carros

--- TestDrive/TestDrive/App.xaml.cs
+++ TestDrive/TestDrive/App.xaml.cs
@@ -10,7 +10,7 @@ namespace TestDrive
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new TestDrive.MainPage();
         }
 
         protected override void OnStart()
